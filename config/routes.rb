ba0d6777bcdc19889373cfc4fe025ec7b8a09627
Zach Ahn <engineering@zachahn.com>
2019-09-10 16:52:05 -0400

    Set up redirects

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root to: redirect("admin/members")
+
   namespace :admin do
     resources :members
     resources :ships
+
+    root to: redirect("admin/members")
   end
 end
