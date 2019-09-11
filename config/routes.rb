4a299d951d2b9e9f712563be79eb4a8379cdd989
Zach Ahn <engineering@zachahn.com>
2019-09-11 20:10:51 +0200

    Set to temporary redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root to: redirect("admin/members")
+  root to: redirect("admin/members", status: 302)
 
   namespace :admin do
     resources :members
     resources :ships
 
-    root to: redirect("admin/members")
+    root to: redirect("admin/members", status: 302)
   end
 end
