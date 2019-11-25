cc41976cb564b91b0973a2141e2db59078a34fb2
Zach Ahn <engineering@zachahn.com>
2019-11-24 19:12:47 -0500

    Update to use Super v0.0.2
    Super 0.0.2 includes several breaking changes from v0.0.1.
    
    This commit encompasses all of the changes that need to be made in order
    to upgrade. See the Super changelog for more details.

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -2,8 +2,55 @@ module Admin
   class MembersController < AdminController
     private
 
-    def dashboard
-      MemberDashboard.new
+    def new_controls
+      Controls.new
+    end
+
+    class Controls
+      def title
+        Member.name.pluralize
+      end
+
+      def model
+        Member
+      end
+
+      def scope(action:)
+        if action.write? || action.delete?
+          raise Super::Error::Forbidden
+        end
+
+        Member.all
+      end
+
+      def permitted_params(params, action:)
+        params.require(:member).permit(:name, :rank, :position, :ship_id)
+      end
+
+      def display_schema(action:)
+        Super::Schema.new(Super::Display::SchemaTypes.new) do |fields, type|
+          fields[:name] = type.dynamic(&:itself)
+          fields[:rank] = type.dynamic(&:itself)
+          fields[:position] = type.dynamic(&:itself)
+          fields[:ship] = type.dynamic { |ship| "#{ship.name} (Ship ##{ship.id})" }
+          fields[:created_at] = type.dynamic(&:iso8601)
+          if action.show?
+            fields[:updated_at] = type.dynamic(&:iso8601)
+          end
+        end
+      end
+
+      def form_schema(action:)
+        Super::Schema.new(Super::Form::SchemaTypes.new) do |fields, type|
+          fields[:name] = type.generic("form_generic_text")
+          fields[:rank] = type.generic("form_generic_select", collection: Member.ranks.keys)
+          fields[:position] = type.generic("form_generic_text")
+          fields[:ship_id] = type.generic(
+            "form_generic_select",
+            collection: Ship.all.map { |s| ["#{s.name} (Ship ##{s.id})", s.id] },
+          )
+        end
+      end
     end
   end
 end
--- app/controllers/admin/ships_controller.rb
+++ app/controllers/admin/ships_controller.rb
@@ -2,8 +2,50 @@ module Admin
   class ShipsController < AdminController
     private
 
-    def dashboard
-      ShipDashboard.new
+    def new_controls
+      Controls.new
+    end
+
+    class Controls
+      def title
+        Ship.name.pluralize
+      end
+
+      def model
+        Ship
+      end
+
+      def scope(action:)
+        if action.write? || action.delete?
+          raise Super::Error::Forbidden
+        end
+
+        Ship.all
+      end
+
+      def permitted_params(params, action:)
+        params.require(:ship).permit(:name, :registry, :class_name)
+      end
+
+      def display_schema(action:)
+        Super::Schema.new(Super::Display::SchemaTypes.new) do |fields, type|
+          fields[:name] = type.dynamic(&:itself)
+          fields[:registry] = type.dynamic(&:itself)
+          fields[:class_name] = type.dynamic(&:itself)
+          if action.show?
+            fields[:created_at] = type.dynamic(&:iso8601)
+            fields[:updated_at] = type.dynamic(&:iso8601)
+          end
+        end
+      end
+
+      def form_schema(action:)
+        Super::Schema.new(Super::Form::SchemaTypes.new) do |fields, type|
+          fields[:name] = type.generic("form_generic_text")
+          fields[:registry] = type.generic("form_generic_text")
+          fields[:class_name] = type.generic("form_generic_text")
+        end
+      end
     end
   end
 end
